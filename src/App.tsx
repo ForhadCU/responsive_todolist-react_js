9329ceb1e86b3e739be87af17388c7aeb36240a9
ForhadCU <kashem.tipo@gmail.com>
2024-06-14 21:01:09 +0600

    Important datatypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  // dataTypes
+  let name: string;
+  let intNumber: number;
+  let boolVar: boolean;
+  let stringArray: string[];
+  let role: [number, string];
+  type Person = {
+    name: string;
+    age: number;
+    isMarried?: boolean; //optional/nullable
+  };
+  let personObjet: Person = {
+    name: "Mr React",
+    age: 25,
+  };
+  let personObjetArray: Person[];
 
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  )
+  return <div className="App">Hello world</div>;
 }
 
-export default App
+export default App;
