f94a75293e9cecc43693af57343bf979b453b2cd
ForhadCU <kashem.tipo@gmail.com>
2024-06-14 21:53:52 +0600

    extending the type and interface. I will prefer to extend interfaces.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import "./App.css";
 
 function App() {
-  // Function declaration
-  let funcName: (param1: string, param2: number) => void; // return undefined type
-  let funcName1: (param1: string) => never; // return nothing
-  let funcName2: (param1: number) => string; // return string
+  // inherit the "type" datatype
+  type X = {
+    a: string;
+    b: number;
+  };
+  type Y = X & {
+    c: string;
+    d: number;
+  };
 
+  let yObject: Y = {
+    c: "aNmae",
+    d: 1,
+    a: "aName",
+    b: 2,
+  };
+
+  // inherit the "interface" datatype
+  interface Person {
+    name: string;
+    age?: number;
+  }
+  interface Profession extends Person {
+    companyName: string;
+    year: number;
+    isCurrentlyWork: boolean;
+  }
+
+  let professionObject: Profession = {
+    name: "Mr React",
+    // age is optional so not ovbious to assign
+    companyName: "Company Name",
+    year: 3,
+    isCurrentlyWork: true,
+  };
 
   return <div className="App">Hello world</div>;
 }
