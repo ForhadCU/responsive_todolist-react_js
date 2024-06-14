3cb57f7e5b3425f6dda411e47b7026854d499c93
ForhadCU <kashem.tipo@gmail.com>
2024-06-14 22:01:09 +0600

    inherit the "type" with "interface" and vice-versa

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,24 @@
 import "./App.css";
 
 function App() {
-  // inherit the "type" datatype
+  // inherit the "type" with "interface"
   type X = {
     a: string;
     b: number;
   };
-  type Y = X & {
-    c: string;
-    d: number;
-  };
 
-  let yObject: Y = {
-    c: "aNmae",
-    d: 1,
-    a: "aName",
-    b: 2,
-  };
-
-  // inherit the "interface" datatype
-  interface Person {
+  interface Person extends X {
     name: string;
     age?: number;
   }
-  interface Profession extends Person {
-    companyName: string;
-    year: number;
-    isCurrentlyWork: boolean;
-  }
 
-  let professionObject: Profession = {
-    name: "Mr React",
-    // age is optional so not ovbious to assign
-    companyName: "Company Name",
-    year: 3,
-    isCurrentlyWork: true,
+  // inherit the "interface" with "type"
+  interface Person2 {
+    name: string;
+    age: number;
+  }
+  type Y = Person2 & {
+    a: string;
   };
 
   return <div className="App">Hello world</div>;
